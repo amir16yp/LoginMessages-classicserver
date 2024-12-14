edc48f3b4e0197d168c98ee26a8cc3c96fcbfafb
Amir Y. Perehodnik <myrprhwdnyq@gmail.com>
2024-12-14 14:38:02 +0200

    Initial commit

---
+++ src/LoginMessages.java
@@ -0,0 +1,33 @@
+import net.classicube.api.API;
+import net.classicube.api.JavaPlugin;
+import net.classicube.api.enums.ChatColors;
+import net.classicube.api.event.EventRegistry;
+import net.classicube.api.event.PlayerDisconnectEvent;
+import net.classicube.api.event.PlayerJoinEvent;
+
+public class LoginMessages extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        EventRegistry.registerListener(this);
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    public void onPlayerJoin(PlayerJoinEvent event)
+    {
+        API.getInstance().broadcastMessage(ChatColors.YELLOW + "Welcome, " + ChatColors.AQUA + event.getPlayer().getUsername() + ChatColors.YELLOW + "!");
+        //player.sendMessage(ChatColors.YELLOW + "You have logged into world: " + ChatColors.RED + player.getLevel().getName());
+    }
+
+    public void onPlayerDisconnect(PlayerDisconnectEvent event)
+    {
+        API.getInstance().broadcastMessage(ChatColors.YELLOW + "Goodbye, " + ChatColors.AQUA + event.getPlayer().getUsername() + ChatColors.YELLOW + "!");
+    }
+
+
+}
